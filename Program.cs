5cda672b96d6205ed4ea65d686b15da7652e61b0
JackDraz <jackwillburnett@gmail.com>
2023-05-13 13:07:16 +0100

    Uploading local progress. Initial connect made

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace SpeedrunComBot 
+{
+    public class Program
+    {
+        private DiscordSocketClient _connectInst;
+        public static Task Main(string[] args) => new Program().MainAsync();
+        
+        public async Task MainAsync()
+        {
+            _connectInst = new DiscordSocketClient();
+            _connectInst.Log += Log;
+
+            String token = Environment.GetEnvironmentVariable("SrComBotToken");
+
+            await _connectInst.LoginAsync(TokenType.Bot, token);
+            await _connectInst.StartAsync();
+
+            await Task.Delay(-1);
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
